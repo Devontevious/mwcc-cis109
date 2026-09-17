0962ef90f8585a6980d226a792b2b0f402643cce
Devontevious <devinhartford27@gmail.com>
2026-09-16 20:43:21 -0400

    WEEK 2

--- lab2/mission.py
+++ lab2/mission.py
@@ -10,4 +10,17 @@ gadget_density = int(tool) / int(train)
 mission_seconds = int(min) / 60
 mission_time_remaining = int(min) - int(travel)
 mission_code = f"{name}-{color}-{age}"
-print(mission_code)
+print(f"Mission Code: {mission_code}")
+if int(age) <= 18:
+    adult = False
+else:
+    adult = True
+if int(tool) <= 5:
+    gadgets = False
+else:
+    gadgets = True
+if int(train) >= 0:
+    rookie = True
+else:
+    rookie = False
+print(f"Hello {name}, you are {age} years old and have been training for {train} years. Your favorite color is {color}. You are carrying {tool} gadgets. You have {min} minutes to complete the mission, with {travel} minutes of travel time. Your training percentage is {training_percentage:.2f}% and your gadget density is {gadget_density:.2f}. The mission code is: {mission_code}. You are an adult: {adult}. You have enough gadgets: {gadgets}. You are a rookie: {rookie}. Good Luck. ")
--- week2/temp_convert.py
+++ week2/temp_convert.py
@@ -1,14 +1,21 @@
-print("Welcome To Temp Converter!")
-print("1. Convert F to C")
-print("2. Convert C to F")
-choice = input("1 or 2? ")
-if (int(choice) == 1):
-    temp_f = input("What Is The Temp (F)? ")
-    temp_c = (float(temp_f) - 32) * (5/9)
-    print("The Temp Is " , temp_c , " (C)")  
-elif (int(choice) == 2):
-    ask_c = input("What Is The Temp (C)")
-    ask_f = (float(ask_c) * (9/5)) + 32
-    print("The Temp Is " , (ask_f) , " (F)")
-else:
-    print("Incorrect Number Please Try Again")
+import sys
+while True:
+    print("Welcome To Temp Converter!")
+    print("1. Convert F to C")
+    print("2. Convert C to F")
+    print("3. End")
+    choice = input("1, 2 or 3? ")
+    if (int(choice) == 1):
+        temp_f = input("What Is The Temp (F)? ")
+        temp_c = (float(temp_f) - 32) * (5/9)
+        print("The Temp Is " , temp_c , " (C)")  
+    elif (int(choice) == 2):
+        ask_c = input("What Is The Temp (C)")
+        ask_f = (float(ask_c) * (9/5)) + 32
+        print("The Temp Is " , (ask_f) , " (F)")
+    else:
+        if(int(choice) == 3):
+            print("Goodbye")
+            sys.exit()
+        else:
+            print("Please Enter A Correct Number")
